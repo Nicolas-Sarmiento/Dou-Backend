2d214b23e72dc0073a788ce6a6aa9fa765725088
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2025-04-04 15:48:42 -0500

    (Add) new problem and submission modules to docker-compose and nginx configuration

---
+++ services/problem_service/src/routes/routes.rs
@@ -0,0 +1,9 @@
+use axum::Router;
+use axum::routing::get;
+
+
+pub fn create_router() -> Router {
+    Router::new()
+        .route("/",get(|| async {"Hello problem service"}) )
+}
+
---
+++ services/submission_service/src/main.rs
@@ -0,0 +1,26 @@
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use crate::routes::routes::create_router;
+
+mod handlers;
+mod database;
+mod routes;
+mod models;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    let db_pool = database::init_db().await.expect("Failed to connect to database");
+    
+    let app = create_router().layer(axum::extract::Extension(db_pool));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+
+    let listener = TcpListener::bind(addr).await.expect("Failed to bind address");
+
+    println!("Server running on {}", addr);
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .expect("Server failed");
+}
