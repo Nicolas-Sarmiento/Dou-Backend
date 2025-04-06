f9137fdba049f76b6dbdde3503ecfd5815bb7446
Nicolas Tinjaca <nicolas.tinjaca@uptc.edu.co>
2025-04-06 09:19:22 -0500

    (FIX) get return statement text

--- services/problem_service/src/handlers/get_exercises.rs
+++ services/problem_service/src/handlers/get_exercises.rs
@@ -5,10 +5,23 @@ use axum::{
 };
 use sqlx::{PgPool, Row};
 use crate::models::models::Problem;
+use tokio::fs::read_to_string;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ProblemWithStatement {
+    problem_id: i32,
+    problem_name: String,
+    problem_statement: String, // <- Aquí va el contenido
+    problem_test_cases_url: String,
+    problem_outputs_url: String,
+    problem_memory_mb_limit: i32,
+    problem_time_ms_limit: i32,
+}
 
 pub async fn get_problems(
     Extension(pool): Extension<PgPool>,
-) -> Result<(StatusCode, Json<Vec<Problem>>), StatusCode> {
+) -> Result<(StatusCode, Json<Vec<ProblemWithStatement>>), StatusCode> {
     let query = "
         SELECT
             problem_id,
@@ -22,29 +35,33 @@ pub async fn get_problems(
         ORDER BY problem_id ASC
     ";
 
-    let rows = sqlx::query(query)
-        .fetch_all(&pool)
-        .await;
+    let rows = sqlx::query(query).fetch_all(&pool).await;
 
     match rows {
         Ok(rows) => {
-            let problems: Vec<Problem> = rows
-                .into_iter()
-                .map(|row| Problem {
+            let mut problems = Vec::new();
+
+            for row in rows {
+                let statement_path: String = row.get("problem_statement_url");
+
+                let statement_content = match read_to_string(&statement_path).await {
+                    Ok(content) => content,
+                    Err(_) => String::from("[Error al leer el statement.txt]"),
+                };
+
+                problems.push(ProblemWithStatement {
                     problem_id: row.get("problem_id"),
                     problem_name: row.get("problem_name"),
-                    problem_statement_url: row.get("problem_statement_url"),
+                    problem_statement: statement_content,
                     problem_test_cases_url: row.get("problem_test_cases_url"),
                     problem_outputs_url: row.get("problem_outputs_url"),
                     problem_memory_mb_limit: row.get("problem_memory_mb_limit"),
                     problem_time_ms_limit: row.get("problem_time_ms_limit"),
-                })
-                .collect();
+                });
+            }
 
             Ok((StatusCode::OK, Json(problems)))
         }
-        Err(e) => {
-            Err(StatusCode::INTERNAL_SERVER_ERROR)
-        }
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
