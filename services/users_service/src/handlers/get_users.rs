ccab38e2a00c66d91074af6348203f0c2df829d1
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2025-04-07 12:35:05 -0500

    (Add) Get user by id

--- services/users_service/src/handlers/get_users.rs
+++ services/users_service/src/handlers/get_users.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Extension, Json, http::StatusCode};
+use axum::{extract::{Extension,Path}, Json, http::StatusCode};
 use sqlx::PgPool;
 use sqlx::Row;
 use crate::models::User;
@@ -24,4 +24,28 @@ pub async fn get_users(
         }
     }).collect();
     Ok(Json(users))
+}
+
+pub async fn get_user_by_id(
+    Extension(pool): Extension<PgPool>,
+    Path(id): Path<i32>
+) -> Result<Json<User>, StatusCode> {
+    let q = "SELECT user_id, username, user_password, user_email, user_role FROM users WHERE user_id = $1";
+    let query = sqlx::query(q);
+    let row= query
+        .bind(id)
+        .fetch_one(&pool)
+        .await
+        .map_err( |e| {
+            eprintln!("Database error: {:?}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+         })?;
+    let user =User{
+            user_id: row.get("user_id"),
+            username: row.get("username"),
+            user_password: row.get("user_password"),
+            user_email : row.get("user_email"),
+            user_role : row.get("user_role"),
+        };
+    Ok(Json(user))
 }
--- services/users_service/src/routes/routes.rs
+++ services/users_service/src/routes/routes.rs
@@ -4,7 +4,7 @@ use axum::{
 };
 use crate::handlers::{
     create_users::create_user, 
-    get_users::get_users,
+    get_users::{get_users,get_user_by_id},
     update_users::update_user,
     delete_users::delete_user,
 };
@@ -12,6 +12,7 @@ use crate::handlers::{
 pub fn create_router() -> Router {
     Router::new()
         .route("/", get(get_users) .post(create_user))
+        .route("/{user_id}", get(get_user_by_id)) 
         .route("/{user_id}", put(update_user)) 
         .route("/{user_id}", delete(delete_user)) 
 
